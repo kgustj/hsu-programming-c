b20d987cfe9f402d305e8d19bbe4996f70087740
kgustj <khs050305@naver.com>
2024-06-02 18:16:29 +0800

    aa

--- 11.c
+++ 11.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 
-int main(void) { return 0; }
+// 1
+void get_frac(double f, int *pi, double *pd) {
+    *pi = (int)f;
+    *pd = f - *pi;
+}
+void func1() {
+    int integer;
+    double decimal, input;
+    printf("실수를 입력하시오: ");
+    scanf("%lf", &input);
+    get_frac(input, &integer, &decimal);
+    printf("정수부는 %d입니다.\n", integer);
+    printf("소수부는 %.2lf입니다.\n", decimal);
+};
+
+int main(void) {
+    func1();
+
+    return 0;
+}
